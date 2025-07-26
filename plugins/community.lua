5d3893ed089dc8eb4aacb3fd2e2a042d740948e9
Sofia Flusova <s.flusova@tbank.ru>
2025-07-27 01:44:03 +0700

    add necessary plugins

---
+++ plugins/community.lua
@@ -0,0 +1,32 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {import = "astrocommunity.pack.html-css"},
+  {import = "astrocommunity.pack.json"},
+  {import = "astrocommunity.pack.markdown"},
+  {import = "astrocommunity.pack.php"},
+  {import = "astrocommunity.pack.prisma"},
+  {import = "astrocommunity.pack.python"},
+  {import = "astrocommunity.pack.cpp"},
+  {import = "astrocommunity.pack.cmake"},
+  {import = "astrocommunity.pack.go"},
+  {import = "astrocommunity.pack.golangci-lint"},
+  {import = "astrocommunity.pack.java"},
+  {import = "astrocommunity.pack.kotlin"},
+  {import = "astrocommunity.pack.proto"},
+  {import = "astrocommunity.pack.nginx"},
+  {import = "astrocommunity.pack.sql"},
+  {import = "astrocommunity.pack.yaml"},
+  {import = "astrocommunity.pack.spring-boot"},
+  {import = "astrocommunity.pack.ansible"},
+  {import = "astrocommunity.pack.tailwindcss"},
+  {import = "astrocommunity.pack.typescript"},
+  {import = "astrocommunity.pack.typescript-all-in-one"}
+ 
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+}
